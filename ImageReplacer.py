b23fbba6fd2f74b7b9876e8670ead444b1a6b029
Bentley James Oakes <bentleyjoakes@gmail.com>
2017-09-17 23:35:23 -0400

    Switch to nested comparisons.

--- ImageReplacer.py
+++ ImageReplacer.py
@@ -12,50 +12,63 @@ class ImageCopier:
         self.source_dirs = source_dirs
         self.target_dirs = target_dirs
 
-        self.source_hashes = collections.defaultdict(list)
-        self.target_hashes = collections.defaultdict(list)
-
-    def hash_images(self):
-
-        self.hash_dir(self.source_dirs, self.source_hashes)
-        self.hash_dir(self.target_dirs, self.target_hashes)
-
-
-    def hash_dir(self, dirs, file_dict):
-
-        for d in dirs:
-            files = sorted(os.listdir(d))
-            for l, f in enumerate(files):
-                i_filename = os.path.join(d, f)
-                i = Image.open(i_filename)
-                h = imagehash.average_hash(i)
-                #print(h)
-                print(l/len(files))
-
-                file_dict[h].append([f, i])
+        self.source_hashes = None
+        self.target_hashes = None
 
     def compare_images(self):
+
+        print("Compare images...")
+
+        print("Source dirs: " + str(self.source_dirs))
+        print("Target dirs: " + str(self.target_dirs))
+
+        for source_dir in self.source_dirs:
+            self.source_hashes = self.hash_dir(source_dir)
+
+            for target_dir in self.target_dirs:
+                self.target_hashes = self.hash_dir(target_dir)
+
+                self.compare_hashes()
+
+
+    def hash_dir(self, d):
+        hash_dict = collections.defaultdict(list)
+
+        print("Hashing directory: " + d)
+        files = sorted(os.listdir(d))
+        for index, f in enumerate(files):
+            i_filename = os.path.join(d, f)
+            i = Image.open(i_filename)
+            h = imagehash.average_hash(i)
+            #print(h)
+            print(index/len(files))
+
+            hash_dict[h].append(f)
+
+        return hash_dict
+
+    def compare_hashes(self):
         for k in self.source_hashes.keys():
             if k in self.target_hashes.keys():
                 print("Found match")
 
-                for v in self.source_hashes[k]:
-                    print(v[0])
-                for v in self.target_hashes[k]:
-                    print(v[0])
+                print(self.source_hashes[k])
+
+                # for v in self.source_hashes[k]:
+                #     print(v[0])
+                # for v in self.target_hashes[k]:
+                #     print(v[0])
 
 
 if __name__ == "__main__":
     config = configparser.ConfigParser()
     config.read('config.cfg')
 
-    source_dirs = config.get('ImageReplacer', 'source_dirs')
-    target_dirs = config.get('ImageReplacer', 'target_dirs')
+    s_dirs = config.get('ImageReplacer', 'source_dirs')
+    t_dirs = config.get('ImageReplacer', 'target_dirs')
 
-    source_dirs = [v.strip() for v in source_dirs.split()]
-    target_dirs = [v.strip() for v in target_dirs.split()]
+    s_dirs = [os.path.expanduser(v.strip()) for v in s_dirs.split()]
+    t_dirs = [os.path.expanduser(v.strip()) for v in t_dirs.split()]
 
-    ic = ImageCopier(source_dirs, target_dirs)
-    ic.hash_images()
-
+    ic = ImageCopier(s_dirs, t_dirs)
     ic.compare_images()
