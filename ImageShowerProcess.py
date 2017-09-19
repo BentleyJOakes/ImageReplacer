a122e0a510d4924472afbb77da07871a80789caf
Bentley James Oakes <bentleyjoakes@gmail.com>
2017-09-19 11:48:59 -0400

    Start comparing hashes to skip files.

--- ImageShowerProcess.py
+++ ImageShowerProcess.py
@@ -1,6 +1,10 @@
 from multiprocessing import Process
 import tkinter as tk
 
+from ImageShower import ImageShower
+
+import hashlib
+
 class ImageShowerProcess(Process):
 
     def __init__(self, images_queue):
@@ -8,10 +12,13 @@ class ImageShowerProcess(Process):
         self.images_queue = images_queue
 
         self.root = tk.Tk()
+    #
+    # def __del__(self):
+    #     print("Shutting down image shower worker...")
 
     def run(self):
 
-        print("Starting image shower worker...")
+        #print("Starting image shower worker...")
 
         while True:
 
@@ -20,12 +27,34 @@ class ImageShowerProcess(Process):
             if images is None:
                 break
 
-            source_image, target_image = images
-            print("Worker: " + str(source_image))
+            source_images, target_images = images
 
-    def build_comparison(self, source_file, target_file):
 
-        img_shower = ImageShower(source_file, target_file, master = self.root)
-        img_shower.mainloop()
+            for v1 in source_images:
+                for v2 in target_images:
 
-        raise Exception()
+                    hash1 = self.get_hash(v1)
+                    hash2 = self.get_hash(v2)
+
+                    print(hash1.hexdigest())
+                    print(hash2.hexdigest())
+                    print(hash1.hexdigest() == hash2.hexdigest())
+
+                    #img_shower = ImageShower(v1, v2, master = self.root)
+                    # img_shower.mainloop()
+
+
+    def get_hash(self, image_file):
+        BUF_SIZE = 65536  # lets read stuff in 64kb chunks!
+
+        md5 = hashlib.md5()
+
+        with open(image_file, 'rb') as f:
+            while True:
+                data = f.read(BUF_SIZE)
+                if not data:
+                    break
+
+                md5.update(data)
+
+        return md5
